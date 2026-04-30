57b5b6e165c4f0869a37373f1003473b3f106014
Nivea Stelmam <nivea.stelmam.704@ufrn.edu.br>
2026-04-30 18:09:49 -0300

    entities

---
+++ src/main/java/io/github/niveastelmam/learntrack/models/Progress.java
@@ -0,0 +1,13 @@
+package io.github.niveastelmam.learntrack.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "tb_progress")
+@Data
+public class Progress {
+
+
+}
