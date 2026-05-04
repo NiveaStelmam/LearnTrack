d7f3c35d3f054cee6386cc4e1fe1fb74299ac563
Nivea Stelmam <nivea.stelmam.704@ufrn.edu.br>
2026-05-03 21:30:16 -0300

    rename repository

--- src/main/java/io/github/niveastelmam/learntrack/repositories/ModuleCourseRepository.java
+++ src/main/java/io/github/niveastelmam/learntrack/repositories/ModuleCourseRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ModuleRepository extends JpaRepository<ModuleCourse, Long> {
+public interface ModuleCourseRepository extends JpaRepository<ModuleCourse, Long> {
 }
